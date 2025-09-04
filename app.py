8654189b7570ee6b1dfab8265e087d49a5e64e4f
arijit dey <ar999557@gmail.com>
2025-09-04 10:22:24 +0530

    all

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask,request,render_template
+app=Flask(__name__,template_folder='template')
+@app.route('/')
+def home_page():
+    return render_template("web.html")
+
+
+
+
+if __name__=="__main__":
+ app.run()
